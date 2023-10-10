bcd57cd45c6e63fe26fe5f51588b147437667a03
Lesego Nkosi <nkosilesego8@gmail.com>
2023-10-10 15:17:57 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  print_alphabet - prints alphabet with lowercase and uppercase
+ *  Return: return 0
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
