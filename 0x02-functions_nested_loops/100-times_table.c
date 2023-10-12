dee66bf81ecb3932263163eb8ed632d92ba37d85
Lesego Nkosi <nkosilesego8@gmail.com>
2023-10-11 17:06:15 -0700

    corrections

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,16 +5,16 @@
  */
 void print_times_table(int n)
 {
-	int i, j, k;
+	int h, i, k;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)
+		for (h = 0; h <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			for (i = 0; i <= n; i++)
 			{
-				k = j * i;
-				if (j == 0)
+				k = i * h;
+				if (i == 0)
 				{
 					_putchar(k + '0');
 				} else if (k < 10 && j != 0)
