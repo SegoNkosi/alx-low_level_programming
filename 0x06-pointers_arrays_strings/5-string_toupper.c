1f7d0346d390bdb29e760d9dae88ceb540146b46
Lesego Nkosi <nkosilesego8@gmail.com>
2023-10-18 13:27:17 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - main function
+ *
+ * @n: String input
+ *
+ * Return: dest
+ */
+
+char *string_toupper(char *n)
+{
+	int k;
+
+	k = 0;
+	while (n[k] != '\0')
+	{
+	if (n[k] >= 'a' && n[k] <= 'z')
+	n[k] = n[k] - 32;
+	k++;
+	}
+return (n);
+}
