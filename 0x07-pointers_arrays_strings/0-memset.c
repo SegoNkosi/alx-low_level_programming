c3dfd107bea90aa18cc21aba0cfff4886d2cecc4
Lesego Nkosi <nkosilesego8@gmail.com>
2023-10-23 14:40:00 -0700

    0-memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - function that fills a block of memory with a constant byte
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int j = 0;
+
+	for (; n > 0; j++)
+	{
+		s[j] = b;
+		n--;
+	}
+	return (s);
+}
