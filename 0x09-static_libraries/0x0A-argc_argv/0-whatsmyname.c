cba5242cfe64aa01bb0cadc7dd6c09e3be7ff13a
Lesego Nkosi <nkosilesego8@gmail.com>
2023-10-27 20:28:41 -0700

    0-whatsmyname

---
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - this function prints the name of the program
+ * @argc: number of arguments
+ * @argv: argument array
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
