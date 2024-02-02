2132675dbaabb62701074092667de25fa9d11225
Lesego Nkosi <nkosilesego8@gmail.com>
2024-02-01 16:16:35 -0800

    correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 		return (NULL);
-	for (i = 0; j < size; j++)
+	for (j = 0; j < size; j++)
 		ht->array[j] = NULL;
 
 	return (ht);
