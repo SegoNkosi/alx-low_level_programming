945b31d3f4889678766b386323cc2a04253fea74
Lesego Nkosi <nkosilesego8@gmail.com>
2024-03-20 19:03:48 -0700

    makefile

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
+
+def island_perimeter(grid):
+    """Return the perimiter of an island.
+
+    The grid represents water by 0 and land by 1.
+
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for j in range(height):
+        for k in range(width):
+            if grid[j][k] == 1:
+                size += 1
+                if (k > 0 and grid[j][k - 1] == 1):
+                    edges += 1
+                if (j > 0 and grid[j - 1][k] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
