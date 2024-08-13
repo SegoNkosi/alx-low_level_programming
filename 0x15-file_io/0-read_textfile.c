ef71d35729ffb6e42dc2ce4ecc26b8974e666226
Lesego Nkosi <nkosilesego8@gmail.com>
2024-08-12 17:43:13 -0700

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,13 +16,13 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file_d;
-	ssize_t lenr, len;
+	ssize_t lenr, lenw;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 	file_d = open(filename, O_RDONLY);
-	if (fille_d == -1)
+	if (file_d == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
